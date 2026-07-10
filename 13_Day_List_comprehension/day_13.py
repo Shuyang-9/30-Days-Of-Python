cfaccfeb1463a60b409276a4373ed5faca4deeec
Shuyang-9 <shisy@umich.edu>
2026-07-10 17:38:14 +0800

    Create 13 .py exercises file

---
+++ 13_Day_List_comprehension/day_13.py
@@ -0,0 +1,36 @@
+language = 'python'
+lst = list(language)
+print(lst)
+
+
+# Exercises: Day 13
+print('\n# 1')
+
+
+
+print('\n# 2')
+
+
+
+print('\n# 3')
+
+
+
+print('\n# 4')
+
+
+
+
+print('\n# 5')
+
+
+
+print('\n# 6')
+
+
+
+
+print('\n# 7')
+
+
+
